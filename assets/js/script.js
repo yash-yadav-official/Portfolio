2726e9f908f384ead91996f350b52d621527294a
Sripathi Mohanasundaram <3sripathi@gmail.com>
2022-10-05 22:41:16 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
-/*
+
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
@@ -52,7 +52,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-*/
 
 
 // custom select variables
