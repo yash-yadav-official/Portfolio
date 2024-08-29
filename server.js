ffef6b60f60191574391468807c46f728a9518a5
Abhishek Tatachar <abhishektatachar@macbook-air.local>
2024-08-29 23:07:35 +0530

    adding node and express capabilities

---
+++ server.js
@@ -0,0 +1,23 @@
+import {getLogger} from './logger.js';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const logger = getLogger();
+
+const app = express();
+const port = 3000;
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  logger.info(`Server is now running at http://localhost:${port}`);
+});
