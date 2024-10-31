4165596b1e0e2238bff77fc7aba3255c472137d3
Whatley <whatleyworkspace@gmail.com>
2024-11-01 00:24:29 +0800

    bug fix

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -14,13 +14,11 @@ fetch('./assets/profile.json')
         document.getElementById('linkedin').textContent = data.linkedin;
         document.getElementById('location').textContent = data.location;
         document.getElementById('about').textContent = data.about;
-        document.getElementById('expT1').textContent = data.expT1;
-        document.getElementById('expDes1').textContent = data.expDes1;
-        document.getElementById('expT2').textContent = data.expT2;
-        document.getElementById('expDes2').textContent = data.expDes2;
-        document.getElementById('expT3').textContent = data.expT3;
-        document.getElementById('expDes3').textContent = data.expDes3;
-        document.getElementById('expT4').textContent = data.expT4;
-        document.getElementById('expDes4').textContent = data.expDes4;
+
+        // Loop through experience array
+        data.experience.forEach((exp, index) => {
+            document.getElementById(`expT${index + 1}`).textContent = exp.type;
+            document.getElementById(`expDes${index + 1}`).textContent = exp.description;
+        });
     })
     .catch(error => console.error('Error fetching the JSON file:', error));
